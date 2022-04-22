e670ce2c13e5ab66c7b67e839b9e85849b672d73
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2022-04-22 13:15:26 +0200

    error app deleted

--- uni_info/src/pages/Home.tsx
+++ uni_info/src/pages/Home.tsx
@@ -1,23 +1,22 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import styled from 'styled-components';
-
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
 
 const Home: React.FC = () => {
- 
   return (
-   
-  
     <IonPage>
-      <IonHeader id='home-header'>
+      <IonHeader>
         <IonToolbar>
-          <IonTitle >SA Universities Info</IonTitle>
+          <IonTitle>Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader >
-      
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
         </IonHeader>
-   
+        <ExploreContainer />
       </IonContent>
     </IonPage>
   );
